5e0896b98e3c5ded591dd859df5afde99c73dfb1
telepuzik <elxgato@gmail.com>
2014-12-08 17:02:45 +0300

    Initial commit

---
+++ scripts/widgets/EmptyWidget.js
@@ -0,0 +1,159 @@
+/**
+ * Created by IMELNIKOV on 08.12.2014.
+ */
+//var a = $("");
+$(function(){
+    $.widget ("custom.structure", {
+       _create: function() {
+           var name = this.options.name;
+           this.element
+               .addClass("widget")
+               .text (name)
+       }
+    });
+
+    $.widget ("custom.blogs", {
+        _create: function() {
+            var name = this.options.name;
+            this.element
+                .addClass("widget")
+                .text (name)
+        }
+    });
+
+    $.widget ("custom.hooray", {
+        _create: function() {
+            var name = this.options.name;
+            this.element
+                .addClass("widget")
+                .text (name)
+        }
+    });
+
+    $.widget ("custom.myprofile", {
+        _create: function() {
+            var name = this.options.name;
+            this.element
+                .addClass("widget")
+                .text (name)
+        }
+    });
+
+    $.widget ("custom.interview", {
+        _create: function() {
+            var name = this.options.name;
+            this.element
+                .addClass("widget")
+                .text (name)
+        }
+    });
+
+    $.widget ("custom.officemap", {
+        _create: function() {
+            var name = this.options.name;
+            this.element
+                .addClass("widget")
+                .text (name)
+        }
+    });
+
+    $.widget ("custom.photos", {
+        _create: function() {
+            var name = this.options.name;
+            this.element
+                .addClass("widget")
+                .text (name)
+        }
+    });
+
+    $.widget ("custom.events", {
+        _create: function() {
+            var name = this.options.name;
+            this.element
+                .addClass("widget")
+                .text (name)
+        }
+    });
+
+    $.widget ("custom.newemployees", {
+        _create: function() {
+            var name = this.options.name;
+            this.element
+                .addClass("widget")
+                .text (name)
+        }
+    });
+
+    $.widget ("custom.birthdays", {
+        _create: function() {
+            var name = this.options.name;
+            this.element
+                .addClass("widget")
+                .text (name)
+        }
+    });
+
+    $.widget ("custom.ideas", {
+        _create: function() {
+            var name = this.options.name;
+            this.element
+                .addClass("widget")
+                .text (name)
+        }
+    });
+
+    $.widget ("custom.note", {
+        _create: function() {
+            var name = this.options.name;
+            this.element
+                .addClass("widget")
+                .text (name)
+        }
+    });
+
+    $.widget ("custom.facebook", {
+        _create: function() {
+            var name = this.options.name;
+            this.element
+                .addClass("widget")
+                .text (name)
+        }
+    });
+
+    $.widget ("custom.job", {
+        _create: function() {
+            var name = this.options.name;
+            this.element
+                .addClass("widget")
+                .text (name)
+        }
+    });
+
+    $.widget ("custom.neighbors", {
+        _create: function() {
+            var name = this.options.name;
+            this.element
+                .addClass("widget")
+                .text (name)
+        }
+    });
+
+    $.widget ("custom.portal", {
+        _create: function() {
+            var name = this.options.name;
+            this.element
+                .addClass("widget")
+                .text (name)
+        }
+    });
+
+    $.widget ("custom.services", {
+        _create: function() {
+            var name = this.options.name;
+            this.element
+                .addClass("widget")
+                .text (name)
+        }
+    });
+});
+
